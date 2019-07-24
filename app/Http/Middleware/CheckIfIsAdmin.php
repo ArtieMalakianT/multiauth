37b3ca7127822b86daceb494fb35dab92b7a94c7
Maicon Alex <maiconalexdesouza@gmail.com>
2019-07-24 14:57:26 -0300

    Fix ao redirecionar página de login de admin

--- app/Http/Middleware/CheckIfIsAdmin.php
+++ app/Http/Middleware/CheckIfIsAdmin.php
@@ -5,7 +5,8 @@ namespace App\Http\Middleware;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Support\Facades\Auth;
 
-class Authenticate extends Middleware
+
+class CheckIfIsAdmin extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
@@ -17,7 +18,7 @@ class Authenticate extends Middleware
     {
         if (! $request->expectsJson())
         {
-            return route('login');           
+            return route('admin.login');           
         }
     }
 }
